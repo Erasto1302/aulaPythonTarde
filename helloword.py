9b31f084988c32904f15e64eed0636d15f901cd2
Erasto1302 <142833651+Erasto1302@users.noreply.github.com>
2023-11-30 15:19:07 -0300

    Create helloword.py
    first

---
+++ helloword.py
@@ -0,0 +1,9 @@
+print("Hello Word !") # Mostra na  tela p Hello Word!
+mensagem = "ola, tudo bem?"
+print(mensagem)
+print(mensagem[1])
+print(mensagem[0:4])
+print(mensagem[-1])
+print(mensagem[10:3])
+print(mensagem[3:7])
+print(mensagem[-4:-1])
