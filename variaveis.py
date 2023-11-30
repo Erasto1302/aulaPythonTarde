24533c1617562e6e8272f80242656e82ffc9929d
Erasto1302 <142833651+Erasto1302@users.noreply.github.com>
2023-11-30 16:44:39 -0300

    Update variaveis.py
    Second

--- variaveis.py
+++ variaveis.py
@@ -6,3 +6,19 @@ print(type(nome))
 print(type(numero))
 print(type(troco))
 print(type(is_login))
+nome = "jose"
+numero = 6
+troco = 4.65
+is_login = True
+print(type(nome))
+print(type(numero))
+print(type(troco))
+print(type(is_login))
+nomes = [ "Maria", "João", "Fabio", "Ana", "Zé" ]
+print(type(nomes))
+print(nomes)
+print(nomes.pop())
+print(nomes)
+nomes.append("Ana")
+nomes.append("Lorenza")
+print(nomes)
